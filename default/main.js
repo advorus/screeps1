2c78f5f5665dba87a38cb274798be754eb010684
Ben <benjaminokane1@gmail.com>
2018-06-24 12:59:00 +0100

    third

--- default/main.js
+++ default/main.js
@@ -1,7 +1,9 @@
 var roleHarvester = require('role.harvester');
 
 module.exports.loop = function () {
-
+  if(Game.spawns['Spawn1'].energy>200){
+    Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY], 'Harvester'+Game.time, {memory: {role: 'harvester'}});
+  }
   for(var name in Game.creeps){
     var creep = Game.creeps[name];
 
