5070ce8515131dd34731c9a623ba314541ceb1dc
Ben <benjaminokane1@gmail.com>
2018-06-24 22:47:07 +0100

    s

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -1,6 +1,11 @@
 var roleHarvester = {
   run: function(creep) {
         var source = creep.room.find(FIND_SOURCES);
+        var extensions = _.filter(creep.room.find(FIND_SOURCES), {
+          filter: function(object)
+            {return object.structureType == STRUCTURE_EXTENSION && object.energy<object.energyCapacity}
+          });
+
         if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity){
           if(creep.carry.energy<creep.carryCapacity){
               if(creep.harvest(source[0]) == ERR_NOT_IN_RANGE){
@@ -8,9 +13,14 @@ var roleHarvester = {
               }
           }
           else{
-              if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                  creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}})
-              }
+                if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}})
+                }
+          }
+        }
+        else if(extensions.length){
+          if(creep.transfer(extensions[0])==ERR_NOT_IN_RANGE){
+            creep.moveTo(extensions[0]);
           }
         }
     }
