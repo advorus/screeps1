7b1cfa37976fb88e55d8263c8229a31c0f3725ee
Ben <benjaminokane1@gmail.com>
2018-06-25 00:44:41 +0100

    s

--- default/role.builder.js
+++ default/role.builder.js
@@ -1,6 +1,6 @@
 var roleBuilder = {
   run: function(creep){
-    var extensions = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION)}});
+    var extensions = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION && structure.energy>0)}});
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
@@ -34,10 +34,11 @@ var roleBuilder = {
             }
 	    }
 	    else {
+        if(extensions.length>3){
             if(creep.withdraw(extensions[1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(extensions[1], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-
+        }
 	    }
 	}
 };
--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -9,8 +9,10 @@ var roleUpgrader = {
               }
           }
           else{
-            if(creep.withdraw(extensions[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-              creep.moveTo(extensions[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(extensions.length>3){
+              if(creep.withdraw(extensions[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                creep.moveTo(extensions[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+              }
             }
           }
     }
