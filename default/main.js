53d5b51df8dca83525b39fcddbf8652ff33ae023
Ben <benjaminokane1@gmail.com>
2018-06-24 14:35:07 +0100

    s

--- default/main.js
+++ default/main.js
@@ -18,15 +18,15 @@ module.exports.loop = function () {
   console.log(harvestCount);
 
   //Spawn in new creeps
-  if(harvestCount<3){
+  if(harvestCount.length<3){
     Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY,WORK], 'Harvester'+Game.time, {memory: {role: 'harvester'}});
   }
 
-  if(upgraderCount<1){
+  if(upgraderCount.length<1){
     Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY,WORK], 'Upgrader'+Game.time, {memory: {role: 'upgrader'}});
   }
 
-  if(builderCount<2){
+  if(builderCount.length<2){
     Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY,WORK], 'Builder'+Game.time, {memory: {role: 'builder'}});
   }
 
