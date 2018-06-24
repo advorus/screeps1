af703e99123c2e76beb7eac8ebd46f42bdefff08
Ben <benjaminokane1@gmail.com>
2018-06-24 23:51:05 +0100

    s

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -12,13 +12,13 @@ var roleHarvester0 = {
                   creep.moveTo(source[0], {visualizePathStyle: {stroke: '#ffaa00'}});
               }
           }
-          else{
+          else if(Game.spawns['Spawn1'].energy<Game.spawns['Spawn1'].energyCapacity){
                 if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
                     creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}})
                 }
           }
         }
-        else if(extensions.length){
+        else {
           if(creep.transfer(extensions[0])==ERR_NOT_IN_RANGE){
             creep.moveTo(extensions[0]);
           }
