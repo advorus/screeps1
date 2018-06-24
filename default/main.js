0352649b28a7e7d607ff189db9f1f5c6c190d365
Ben <benjaminokane1@gmail.com>
2018-06-24 13:14:00 +0100

    a

--- default/main.js
+++ default/main.js
@@ -1,4 +1,5 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
   for(var i in Memory.creeps){
@@ -7,10 +8,13 @@ module.exports.loop = function () {
     }
   }
 
-  var HarvestCount = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  
-  if(HarvestCount<3){
+  var harvestCount = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  var upgraderCount = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  if(harvestCount<3){
     Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY], 'Harvester'+Game.time, {memory: {role: 'harvester'}});
+  }
+  if(upgraderCount<3){
+    Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY], 'Upgrader'+Game.time, {memory: {role: 'upgrader'}});
   }
 
   for(var name in Game.creeps){
@@ -18,6 +22,9 @@ module.exports.loop = function () {
 
     if(creep.memory.role == 'harvester'){
       roleHarvester.run(creep);
+    }
+    if(creep.memory.role == 'upgrader'){
+      roleUpgrader.run(creep);
     }
   }
 }
