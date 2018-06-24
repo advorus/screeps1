c86ba7da322ca0056a122af8f609cdff0b4ec557
Ben <benjaminokane1@gmail.com>
2018-06-25 00:06:31 +0100

    s

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -10,16 +10,17 @@ var roleHarvester0 = {
               }
           }
           else if(Game.spawns['Spawn1'].energy<Game.spawns['Spawn1'].energyCapacity){
-                if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}})
-                }
+              if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                  creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}})
+              }
           }
-        }
-        else {
-          if(creep.transfer(extensions[0], RESOURCE_ENERGY)==ERR_NOT_IN_RANGE){
-            creep.moveTo(extensions[0]);
+
+          else {
+            if(creep.transfer(extensions[0], RESOURCE_ENERGY)==ERR_NOT_IN_RANGE){
+              creep.moveTo(extensions[0]);
+            }
           }
-        }
+      }
     }
   };
 
