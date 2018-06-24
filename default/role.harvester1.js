dc1953bf3ada4c61975134ffe5116a8a34fe1e2b
Ben <benjaminokane1@gmail.com>
2018-06-25 00:02:20 +0100

    s

--- default/role.harvester1.js
+++ default/role.harvester1.js
@@ -3,7 +3,9 @@ var roleHarvester1 = {
         var source = creep.room.find(FIND_SOURCES);
         var extensions = _.filter(creep.room.find(FIND_SOURCES), {
           filter: function(object)
-            {return object.structureType == STRUCTURE_EXTENSION && object.energy<object.energyCapacity}
+            {if(object.structureType == STRUCTURE_EXTENSION && object.energy<object.energyCapacity){
+              return true;
+            }}
           });
 
         if(creep.room.energyAvailable < creep.room.energyCapacityAvailable){
