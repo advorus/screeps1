5a1beb4f698385968c264a6b0cf95acba5ef21c5
Ben <benjaminokane1@gmail.com>
2018-06-26 18:41:25 +0100

    s

--- default/main.js
+++ default/main.js
@@ -1,3 +1,7 @@
+var memoryClear = require('memoryClear');
+var creepSpawner = require('creepSpawner');
+var assignCreeps = require('assignCreeps');
+
 module.exports.loop = function () {
   memoryClear.creeps();
   creepSpawner.spawn();
