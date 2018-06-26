c2cbd5e413c26aa65b39254ed9aa7d8da9af4461
Ben <benjaminokane1@gmail.com>
2018-06-26 12:29:48 +0100

    s

--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -9,11 +9,17 @@ var roleUpgrader = {
               }
           }
           else{
-
+            if(containers.length){
               if(creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
                 creep.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffaa00'}});
               }
             }
+            else{
+              if(creep.harvest(source[0]==ERR_NOT_IN_RANGE){
+                creep.moveTo(source[0]);
+              }
+            }
+          }
 
     }
   };
